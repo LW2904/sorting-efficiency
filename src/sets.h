847e58b805c0356940838b73e5216484d11ea878
fsoc <root@fsoc.space>
2020-07-28 19:56:01 +0200

    Move random_shuffle to utils

--- src/sets.h
+++ src/sets.h
@@ -1,21 +1,12 @@
 #pragma once
 
+#include "utils.h"
+
 #include <vector>	// vector
-#include <random>	// random_device, mt19937
 #include <cstddef>	// size_t
 #include <numeric>	// iota
-#include <algorithm>	// partial_sort, random_shuffle
 
 namespace sets {
-
-// Since std::random_shuffle is deprecated
-template<class I>
-static void random_shuffle(I first, I last) {
-	std::random_device rd;
-	std::mt19937 g(rd());
-
-	std::shuffle(first, last, g);
-}
 
 std::vector<int> sorted(const size_t size) {
 	auto set = std::vector<int>(size);
@@ -28,18 +19,7 @@ std::vector<int> sorted(const size_t size) {
 std::vector<int> random(const size_t size) {
 	auto set = sorted(size);
 
-	sets::random_shuffle(set.begin(), set.end());
-
-	return set;
-}
-
-std::vector<int> partially_sorted(const size_t size) {
-	auto set = sorted(size);
-
-	std::random_device rd;
-	std::mt19937 g(rd());
-
-	sets::random_shuffle(set.begin(), set.begin() + (size * 0.8));
+	utils::random_shuffle(set.begin(), set.end());
 
 	return set;
 }
---
+++ src/utils.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>	// shuffle
+#include <random>	// random_device, mt19937
+
+namespace utils {
+
+// Since std::random_shuffle is deprecated
+template<class I>
+static void random_shuffle(I first, I last) {
+	std::random_device rd;
+	std::mt19937 g(rd());
+
+	std::shuffle(first, last, g);
+}
+
+}
