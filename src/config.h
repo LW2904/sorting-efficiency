d14f63765e7f58371f16187bf67be23e2ef1f6cd
fsoc <root@fsoc.space>
2020-08-07 15:11:21 +0200

    Remove unimplemented options

--- src/config.h
+++ src/config.h
@@ -8,9 +8,7 @@ class config {
 "Usage: sorting [options]\n"
 "Options:\n"
 "  -h, --help        Display this information and exit.\n"
-"  -s, --strategy    Sets the benchmarking strategy. Valid parameters are \"human\"\n"
-"                    and \"gnuplot\". Currently, only the latter is supported.\n"
-"  -o, --output      Sets the output path if the strategy is set to \"gnuplot\".\n"
+"  -o, --output      Sets the output path.\n"
 		);
 	}
 
@@ -20,21 +18,10 @@ class config {
 			return print_help();
 		}
 
-		std::string _strategy;
-		cmd("strategy") >> _strategy;
-		if (_strategy == "human") {
-			strategy = strategy::human;
-		} else if (_strategy == "gnuplot") {
-			strategy = strategy::gnuplot;
-		}
-
 		cmd("output") >> output;
 	}
 
 public:
-	enum class strategy { human, gnuplot };
-	strategy strategy = strategy::human;
-
 	std::string output = "out/";
 
 	bool should_exit = false;
