c3049d40e10fc1b1c5e31ea864188a39f7298613
fsoc <root@fsoc.space>
2021-06-06 01:03:43 +0200

    Refactor subset size generator

--- src/benchmark.h
+++ src/benchmark.h
@@ -4,6 +4,7 @@
 #include "sorters.h"
 #include "experiment.h"
 
+#include <map>
 #include <cmath>
 #include <cfenv>
 #include <cstdio>	// printf
@@ -19,23 +20,35 @@ namespace benchmark {
 
 	enum step_type_t { linear, quadratic };
 
-	auto get_step_generator(const size_t set_size, const size_t total_chunks,
-		step_type_t step_type
+	namespace detail {
+		// A version of pow from cmath that only works fo y = 1 and y = 2.
+		// Implemented because regular pow _is not constexpr_ for whatever
+		// reason. It's stupid that this needs to exist.
+		constexpr auto pow2(const size_t x, const size_t y) {
+			return y == 1 ? x : y == 2 ? x * x : 0;
+		}
+	}
+
+	// Returns a function f, modeling the subset sizes given the total set size
+	// and the requested amount of chunks. Since f should be thought of as a
+	// function in the mathematical sense, the first subset size is f(1).
+	// Values of f are always positive and always <= set_size.
+	constexpr auto get_subset_size_factory(const size_t set_size, const size_t total_chunks,
+		const step_type_t step_type
 	) {
-		std::fesetround(FE_TONEAREST);
+		const auto power = step_type == linear ? 1 : 2;
 
-		// Determine the `a` in a function of the type f(x) = ax^2 + b where
-		// f(total_chunks) = set_size. 
-		const double a = set_size > total_chunks ? (
-			std::nearbyint((
-				step_type == quadratic ? sqrt(set_size) : set_size
-			) / total_chunks)
-		) : 1;
+		// Determine the `a` in a function of the type f(x) = ax (for
+		// the linear step type) or f(x) = ax^2 (for the quadratic one).
+		const int a = set_size > total_chunks ? ((int)(
+			set_size / detail::pow2(total_chunks, power)
+		)) : 1;
 
-		// Return a lambda which models f
-		return [a, step_type](size_t i) {
-			const auto power = step_type == quadratic ? 2 : 1;
-			return (std::ptrdiff_t)(pow(a * double(i + 1), power));
+		// Return a lambda which models f.
+		return [a, power, set_size](const size_t i) constexpr {
+			const auto value = a * detail::pow2(i, power);
+
+			return value < 0 ? 0 : value >= set_size ? set_size : value;
 		};
 	}
 
@@ -44,11 +57,11 @@ namespace benchmark {
 	) {
 		timings_t timings;
 
-		const auto step_generator = get_step_generator(set.size(),
+		const auto get_subset_size = get_subset_size_factory(set.size(),
 			total_chunks, step_type);
 
-		for (int i = 0; i < total_chunks; i++) {
-			auto subset_size = step_generator(i);
+		for (int i = 1; i <= total_chunks; i++) {
+			auto subset_size = (std::ptrdiff_t)get_subset_size(i);
 
 			auto subset = sets::set_t(set.begin(), set.begin() + subset_size);
 			const auto time = experiment([&]() {
